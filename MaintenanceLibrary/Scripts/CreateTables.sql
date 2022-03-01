94c25f1ee1fb3a4d37e93c56d8c7ff2f53affe69
alice <dubinaalisa1@gmail.com>
2022-03-01 16:27:36 +0300

    Start of project

---
+++ MaintenanceLibrary/Scripts/CreateTables.sql
@@ -0,0 +1,154 @@
+﻿-- Создание таблиц базы данных "Техобслуживание"
+
+-- удаление существующих таблиц
+drop table if exists BrakeFacts;
+drop table if exists RepairFacts;
+drop table if exists Malfunctions;
+drop table if exists [Services];
+drop table if exists Cars;
+drop table if exists Colors;
+drop table if exists BrandModels;
+drop table if exists Workers;
+drop table if exists Clients;
+drop table if exists Persons;
+drop table if exists Specialties;
+go
+
+
+-- Таблица -  справочник специальностей работников
+create table dbo.Specialties (
+	Id          int          not null primary key identity (1, 1),
+	Speciality  nvarchar(40) not null    -- название специальности
+);
+go
+
+
+-- Таблица персональных данных, одинаковых для работников 
+-- и клиентов - Persons
+create table dbo.Persons (
+	Id          int          not null primary key identity (1, 1),
+	Passport    nvarchar(15) not null,    -- Серия и номер паспорта персоны
+	Surname     nvarchar(60) not null,    -- Фамилия персоны
+	[Name]      nvarchar(50) not null,    -- Имя персоны
+	Patronymic  nvarchar(60) not null     -- Отчество персоны
+);
+go
+
+
+-- Таблица сведений о клиентах
+create table dbo.Clients (
+	Id              int           not null primary key identity (1, 1),
+	IdPerson        int           not null,    -- Внешний ключ, связь с персональными данными
+	BornDate        date          not null,    -- Дата рождения 
+	ResidencePermit nvarchar(255) not null     -- Прописка 
+	
+	-- внешний ключ - связь 1:1 к таблице Persons
+	constraint  FK_Clients_Persons foreign key (IdPerson) references dbo.Persons(Id)
+);
+go
+
+
+-- Таблица сведений о работниках
+create table Workers(
+    Id             int          not null primary key identity(1, 1),
+	IdPerson       int          not null,  -- Внешний ключ, связь с персональными данными
+	IdSpecialty    int          not null,  -- Внешний ключ, связь с специальностями
+	Category       nvarchar(60) not null,  -- Разряд
+	Salary         float        not null,  -- Должностной оклад
+	WorkExperience int          not null,  -- Стаж работы
+	IsDismissed    bit          not null,  -- Признак уволен (true - уволен)
+
+	constraint  FK_Workers_Persons     foreign key (IdPerson)    references dbo.Persons(Id),
+	constraint  FK_Workers_Specialties foreign key (IdSpecialty) references dbo.Specialties(Id)
+);
+go
+
+
+-- Названия моделей автомобилей, включая бренд
+create table BrandModels (
+	Id          int          not null primary key identity (1, 1),
+	BrandModel  nvarchar(30) not null    -- название бренда, модели
+);
+go
+
+
+-- Названия цветов автомобилей
+create table Colors (
+	Id     int          not null primary key identity (1, 1),
+	Color  nvarchar(30) not null    -- название цвета
+);
+go
+
+-- Таблица автомобилей 
+create table dbo.Cars (
+    Id          int          not null primary key identity (1, 1),
+	IdBrand     int          not null, -- внешний ключ, ссылка на BravdModels, бренд-модель автомобиля
+	IdColor     int          not null, -- внешний ключ, ссылка на Colors, цвет автомобиля
+	IdClient    int          not null, -- внешний ключ, ссылка на Clients, владелец автомобиля
+	Plate       nvarchar(12) not null, -- гос. регистрационный номер автомобиля
+	YearManuf   int          not null, -- год производства автомобиля
+
+	-- ограничение уникальности значения столбца
+	constraint  CK_Cars_Plate       unique(Plate),
+	
+	-- внешние ключи
+	constraint  FK_Cars_Clients foreign key (IdClient) references dbo.Clients(Id), 
+	constraint  FK_Cars_BrandModels foreign key (IdBrand) references dbo.BrandModels(Id), 
+	constraint  FK_Cars_Colors foreign key (IdColor) references dbo.Colors(Id) 
+);
+go
+
+
+-- Таблица услуг, оказываемых станцией
+create table dbo.[Services] (
+    Id             int          not null primary key identity (1, 1),
+	[Service]      nvarchar(80) not null, -- название услуги
+	WorkCost       int          not null, -- стоимость услуги
+	SparePartsCost int          not null, -- стоимость запчастей
+
+    -- ограничения полей таблицы
+	constraint CK_Services_WorkCost       check (WorkCost > 0),
+	constraint CK_Services_SparePartsCost check (SparePartsCost >= 0)
+);
+go
+
+
+-- Таблица неисправностей
+create table Malfunctions (
+	Id           int          not null primary key identity (1, 1),
+	Malfunction  nvarchar(50) not null    -- описание неисправности
+);
+go
+
+
+-- Таблица фактов ремонта
+create table dbo.RepairFacts (
+    Id          int          not null primary key identity (1, 1),
+	IdCar       int          not null, -- внешний ключ, ссылка на Cars, автомобиль
+	IdClient    int          not null, -- внешний ключ, ссылка на Clients (Клиент сдает в ремонт не обязательно автомобиль, владельцем которого он является)
+	DateStart   date         not null, -- дата начала ремонта
+    DateFinish  date         not null, -- дата возврата автомобиля
+	
+	-- внешние ключи
+	constraint  FK_RepairFacts_Cars foreign key (IdCar) references dbo.Cars(Id), 
+	constraint  FK_RepairFacts_Clients foreign key (IdClient) references dbo.Clients(Id), 
+);
+go
+
+
+-- Таблица фактов поломки
+create table dbo.BrakeFacts (
+    Id            int          not null primary key identity (1, 1),
+	IdMalfunction int          not null, -- внешний ключ, ссылка на Malfunctions, неисправность
+	IdWorker      int          not null, -- внешний ключ, ссылка на Workers, работник
+	IdRepairFact  int          not null, -- внешний ключ, ссылка на RepairFacts, факт ремонта
+	IdService     int          not null, -- внешний ключ, ссылка на Services, улуга
+	[DateTime]    datetime     not null, -- время устранения неисправности
+	
+	-- внешние ключи
+	constraint  FK_BrakeFacts_RepairFacts foreign key (IdRepairFact) references dbo.RepairFacts(Id), 
+	constraint  FK_BrakeFacts_Workers foreign key (IdWorker) references dbo.Workers(Id), 
+	constraint  FK_BrakeFacts_Malfunctions foreign key (IdMalfunction) references dbo.Malfunctions(Id), 
+	constraint  FK_BrakeFacts_Services foreign key (IdService) references dbo.[Services](Id), 
+);
+go
---
+++ MaintenanceLibrary/Scripts/Queries.sql
@@ -0,0 +1,231 @@
+﻿/* База данных «Техобслуживание»  */
+
+-- справочник моделей автомобилей, включая бренд
+select
+   *
+from
+    BrandModels;
+go
+
+-- справочник цветов автомобилей
+select
+    *
+from
+    Colors;
+go
+
+-- справочник специальностей работников
+select
+    *
+from
+    Specialties;
+go
+
+-- персональные данные
+select
+    *
+from
+    Persons;
+go
+
+-- справочник неисправностей
+select
+    *
+from
+    Malfunctions;
+go
+
+-- услуги, оказываемые станцией
+select
+    *
+from
+    [Services];
+go
+
+-- клиенты
+select
+   Clients.Id
+   , Persons.Surname
+   , Persons.[Name]
+   , Persons.Patronymic
+   , Persons.Passport
+   , Clients.BornDate
+   , Clients.ResidencePermit
+from
+    Clients join Persons on Clients.IdPerson = Persons.Id;
+go
+
+-- работники
+select
+   Workers.Id
+   , Persons.Surname
+   , Persons.[Name]
+   , Persons.Patronymic
+   , Persons.Passport
+   , Specialties.Speciality
+   , Workers.Category
+   , Workers.Salary
+   , Workers.WorkExperience
+from
+    Workers join Persons on Workers.IdPerson = Persons.Id
+            join Specialties on Workers.IdSpecialty = Specialties.Id;
+go
+
+-- автомобили
+select
+   Cars.Id
+   , Persons.Surname + N' ' + Substring(Persons.[Name], 1, 1) + N'.' + Substring(Persons.Patronymic, 1, 1) + N'.' as [Owner]
+   , BrandModels.BrandModel
+   , Colors.Color
+   , Cars.Plate
+   , Cars.YearManuf
+from
+    Cars join (Clients join Persons on Clients.IdPerson = Persons.Id) on Cars.IdClient = Clients.Id
+         join BrandModels on Cars.IdBrand = BrandModels.Id
+         join Colors on Cars.IdColor = Colors.Id;
+go
+
+-- факты ремонта
+select
+   RepairFacts.Id
+   , Cars.Plate as Car
+   , Persons.Surname + N' ' + Substring(Persons.[Name], 1, 1) + N'.' + Substring(Persons.Patronymic, 1, 1) + N'.' as Client
+   , RepairFacts.DateStart
+   , RepairFacts.DateFinish
+from
+    RepairFacts join (Clients join Persons on Clients.IdPerson = Persons.Id) on RepairFacts.IdClient = Clients.Id
+                join Cars on RepairFacts.IdCar = Cars.Id;
+go
+
+-- факты поломки
+select
+   BrakeFacts.Id
+   , Cars.Plate as Car
+   , Malfunctions.Malfunction
+   , [Services].[Service]
+   , Persons.Surname + N' ' + Substring(Persons.[Name], 1, 1) + N'.' + Substring(Persons.Patronymic, 1, 1) + N'.' as Worker
+from
+    BrakeFacts join (Workers join Persons on Workers.IdPerson = Persons.Id) on BrakeFacts.IdWorker = Workers.Id
+               join (RepairFacts join Cars on RepairFacts.IdCar = Cars.Id) on BrakeFacts.IdRepairFact = RepairFacts.Id
+               join Malfunctions on BrakeFacts.IdMalfunction = Malfunctions.Id
+               join [Services] on BrakeFacts.IdService = [Services].Id;
+go
+
+-- запросы по заданию
+
+-- Запрос 1. Фамилия, имя, отчество и адрес владельца автомобиля
+--           с данным номером государственной регистрации.
+declare @plate nvarchar(12) = N'О169РК';
+select
+   Cars.Plate
+   , Persons.Surname 
+   , Persons.[Name]
+   , Persons.Patronymic
+   , Clients.ResidencePermit
+from
+    Cars join (Clients join Persons on Clients.IdPerson = Persons.Id) on Cars.IdClient = Clients.Id
+where
+    Cars.Plate = @plate;
+go
+    
+
+-- Запрос 2. Марка и год выпуска автомобиля данного владельца.
+declare @passport nvarchar(12) = N'12 21 345671'; -- серия и номер паспорта владельца
+select
+   Cars.Plate
+   , BrandModels.BrandModel
+   , Cars.YearManuf
+from
+    Cars join (Clients join Persons on Clients.IdPerson = Persons.Id) on Cars.IdClient = Clients.Id
+         join BrandModels on Cars.IdBrand = BrandModels.Id
+where
+    Persons.Passport = @passport;
+go
+
+
+-- Запрос 3. Перечень устраненных неисправностей в автомобиле данного владельца.
+declare @passport nvarchar(12) = N'12 21 345671'; -- серия и номер паспорта владельца
+select
+   BrakeFacts.Id
+   , Cars.Plate as Car
+   , Malfunctions.Malfunction
+from
+    BrakeFacts join (RepairFacts join (Cars join (Clients join Persons on Clients.IdPerson = Persons.Id) on Cars.IdClient = Clients.Id) 
+                                                   on RepairFacts.IdCar = Cars.Id) on BrakeFacts.IdRepairFact = RepairFacts.Id
+               join Malfunctions on BrakeFacts.IdMalfunction = Malfunctions.Id
+where 
+    Persons.Passport = @passport;
+go
+
+-- Запрос 4. Фамилия, имя, отчество работника станции, устранявшего 
+--           данную неисправность в автомобиле данного клиента, 
+--           и время ее устранения.
+declare @passport nvarchar(12) = N'12 21 345671', -- серия и номер паспорта клиента
+        @malfunction nvarchar(50) = N'Неисправность двигателя'; -- неисправность
+
+select
+   Cars.Plate as Car
+   , Malfunctions.Malfunction
+   , Persons.Surname as WorkerSurname
+   , Persons.[Name] as WorkerName
+   , Persons.Patronymic as WorkerPatronymic
+   , BrakeFacts.[DateTime]
+from
+    BrakeFacts join (Workers join Persons on Workers.IdPerson = Persons.Id) on BrakeFacts.IdWorker = Workers.Id
+               join (RepairFacts join Cars on RepairFacts.IdCar = Cars.Id) on BrakeFacts.IdRepairFact = RepairFacts.Id
+               join Malfunctions on BrakeFacts.IdMalfunction = Malfunctions.Id
+               join (RepairFacts R join (Clients join Persons P on Clients.IdPerson = P.Id)
+                      on R.IdClient = Clients.Id) on BrakeFacts.IdRepairFact = R.Id
+where 
+    P.Passport = @passport and Malfunctions.Malfunction = @malfunction;
+go
+
+-- Запрос 5. Фамилия, имя, отчество клиентов, сдавших в ремонт 
+--           автомобили с указанным типом неисправности.
+declare @malfunction nvarchar(50) = N'Неисправность двигателя';
+select
+   Cars.Plate as Car
+   , Persons.Surname 
+   , Persons.[Name]
+   , Persons.Patronymic
+   , Malfunctions.Malfunction
+from
+    BrakeFacts join (RepairFacts join (Clients join Persons on Clients.IdPerson = Persons.Id)
+                      on RepairFacts.IdClient = Clients.Id) on BrakeFacts.IdRepairFact = RepairFacts.Id
+               join Malfunctions on BrakeFacts.IdMalfunction = Malfunctions.Id
+               join (RepairFacts Temp join Cars on Temp.IdCar = Cars.Id) on BrakeFacts.IdRepairFact = Temp.Id 
+where 
+    Malfunctions.Malfunction = @malfunction;
+go
+
+-- Запрос 6. Самая распространенная неисправность в автомобилях указанной марки.
+declare @brand nvarchar(30) = N'Volkswagen Polo';
+select
+    BrandModels.BrandModel
+    , Malfunctions.Malfunction
+    , COUNT(*) as Amount
+from
+    BrakeFacts join Malfunctions on BrakeFacts.IdMalfunction = Malfunctions.Id
+               join (RepairFacts join (Cars join BrandModels on Cars.IdBrand = BrandModels.Id) 
+               on RepairFacts.IdCar = Cars.Id) on BrakeFacts.IdRepairFact = RepairFacts.Id 
+where 
+    BrandModels.BrandModel = @brand
+group by
+    BrandModels.BrandModel
+    , Malfunctions.Malfunction
+Order by 
+    Amount desc;
+go
+
+-- Запрос 7. Количество рабочих каждой специальности на станции.
+-- работники
+select
+    Specialties.Speciality
+    , COUNT(*) as Amount
+from
+    Workers join Specialties on Workers.IdSpecialty = Specialties.Id
+group by
+    Specialties.Speciality
+Order by 
+    Amount desc;
+go
